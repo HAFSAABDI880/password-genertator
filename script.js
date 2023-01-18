c4795739966054a234d3ca653ff3a7e63ae1a0f6
HAFSAABDI880 <119974289+HAFSAABDI880@users.noreply.github.com>
2023-01-18 19:45:31 +0000

    adding password lenghth

--- script.js
+++ script.js
@@ -27,18 +27,23 @@ var allCharachters = ""
   const generatePassword= () => {
 
   // get the password lenghth
-   const passwordLength= prompt("How long do you want your password to be")
-   console.log (passwordLength, " password length");
-  var includeUppercase = confirm(" press okay if yuo want to includd uppercase")
-  console.log (includeUppercase )
-  var includeNumber= confirm ( "Do you want to include numbers ")
-  var includeLowercase =confirm ("Do you want to include lower case")
-  var includeSpecialCharacters =confirm ("Do you want to include special charachters")
+const passwordLength= prompt("How long do you want your password to be")
+ console.log (passwordLength, " password length");
+ if( passwordLength< 8) {  
+  alert( "invalid")
+ }
+ if ( passwordLength>128) {
+  alert ( "invalid")
+ }
+  //var includeUppercase = confirm(" press okay if yuo want to includd uppercase")
+  //console.log (includeUppercase )
+  //var includeNumber= confirm ( "Do you want to include numbers ")
+ // var includeLowercase =confirm ("Do you want to include lower case")
+  //var includeSpecialCharacters =confirm ("Do you want to include special charachters")
 
-  if(includeUppercase === true ) {
-    console.log ("The user wants uppercase")
-    console.log( " The user wants lowercase")
-  }
+  //if(includeUppercase === true ) {
+    //console.log ("The user wants uppercase")
+    // console.log( " The user wants lowercase")  }
 // create the password criteria 
   // const passwordCriteria=getpasswordCriteria();
 
