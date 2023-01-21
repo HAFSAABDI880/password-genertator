59d1e8b1d7dc522995ca2420c358aa1686241354
HAFSAABDI880 <119974289+HAFSAABDI880@users.noreply.github.com>
2023-01-21 21:11:04 +0000

    prompt

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
- // Assignment code 
- var generateBtn= document.querySelector("#generate"); 
+
+// Assignment code 
+var generateBtn= document.querySelector("#generate"); 
 
 function generatePassword () {
   const upperCase="ABCDEGHIJKLMNOPQRSTUVYXYZ2";
@@ -7,7 +8,7 @@ function generatePassword () {
   const Numbers=" 0123456789";
   const speical= "!@£$%&*?+=";}
 
-  //get password lenght
+  //get password lenghth
   const passwordLength= Number (prompt("Enter a password lenght:"));
 
   console.log(passwordLength);
@@ -22,7 +23,7 @@ function generatePassword () {
  const isupperCase= confirm (" Do you want uppercase");
  const islowerCase =confirm ( "Do you want uppercase");
  const isNumbers= confirm ( " Do you want a number ");
- const ispecial =confirm (" Dp you want special ");
+ const ispecial =confirm (" Do you want special ");
 
  if (!islowerCase && !isupperCase && isNumbers && !ispecial) {
   alert ();
@@ -42,12 +43,29 @@ function generatePassword () {
  if( isSpecial){
   choices.push (speical);
  }
+let password= "";
+
+
+
+ // generate random password 
+ for ( let i=0; i < 10; i++) { 
+const randomChoiceIndex= Maths.floor (Maths.random( )* choices.length); 
+
+const randomCharacters= choice [randomChoiceIndex];
+
+password += randomCharacters.charAt(
+  Math.floor (Math.random() *randomCharacters.length)
+);
+}
+{return password ;
+}
+
  // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword();8
 var passwordText = document.querySelector("#password");
  passwordText.value = password;
 
 }
  //Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  generateBtnaddEventListener ("click", writePassword);
