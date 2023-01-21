720986a12648af97512d99d1989e556ea722ff98
HAFSAABDI880 <119974289+HAFSAABDI880@users.noreply.github.com>
2023-01-21 17:15:36 +0000

    adding

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
  // Assignment code 
-const generateBtn = document.querySelector("#generate");
+ var generateBtn= document.querySelector("#generate"); 
 
-function generatePassword (){
+function generatePassword () {
   const upperCase="ABCDEGHIJKLMNOPQRSTUVYXYZ2";
   const lowerCase=" abcdefghijklmnopqrstuvwxyxz";
   const Numbers=" 0123456789";
-  const speical= "!@£$%&*?+=";
+  const speical= "!@£$%&*?+=";}
 
   //get password lenght
   const passwordLength= Number (prompt("Enter a password lenght:"));
 
   console.log(passwordLength);
 
+  if ( passwordLength < 8 || passwordLength>128) {
+    alert("error");
+    return passwordLength;
+ } else if (passwordLength >= 8 || passwordLength <=128){
 
+ }
+
+ const isupperCase= confirm (" Do you want uppercase");
+ const islowerCase =confirm ( "Do you want uppercase");
+ const isNumbers= confirm ( " Do you want a number ");
+ const isspecisl =confirm (" Dp you want special ");
+
+
+ // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+var passwordText = document.querySelector("#password");
+ passwordText.value = password;
 
 }
+ //Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
